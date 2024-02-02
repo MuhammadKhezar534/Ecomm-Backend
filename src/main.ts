0f62b45af2f1334a1cac0f9ecfd2fda041424291
MuhammadKhezar534 <muhammadkhezar36@gmail.com>
2024-02-03 00:30:28 +0500

    initial

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: false, transform: false }),
+  );
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+  });
+
+  console.log({port:process.env.PORT })
+
+  await app.listen(process.env.PORT || 3008);
+}
+bootstrap();
