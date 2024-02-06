caa53067e503c50c33dc657f79a225147b6a7d9b
MuhammadKhezar534 <muhammadkhezar36@gmail.com>
2024-02-07 01:23:22 +0500

    changes

--- src/modules/Product/product.interface.ts
+++ src/modules/Product/product.interface.ts
@@ -1,5 +1,8 @@
 export interface Product {
-    _id: any;
-  url:string
+    _id: any,
+  url:string,
+  discount:number,
+  price:number
+
   }
   
--- src/modules/Product/product.schema.ts
+++ src/modules/Product/product.schema.ts
@@ -4,6 +4,8 @@ export const ProductSchema = new mongoose.Schema(
   {
     url: { type: String, default: '' },
     id: { type: mongoose.Schema.Types.ObjectId},
+    discount:{type:Number},
+    price:{type:Number}
   },
   { timestamps: true },
 );
